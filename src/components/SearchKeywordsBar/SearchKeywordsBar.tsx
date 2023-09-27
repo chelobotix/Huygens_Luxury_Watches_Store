990615d4a842d2d5a49ee62b1e82da15d57c5025
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-09-27 13:44:53 -0400

    Add minPrice contidion

--- src/components/SearchKeywordsBar/SearchKeywordsBar.tsx
+++ src/components/SearchKeywordsBar/SearchKeywordsBar.tsx
@@ -1,6 +1,7 @@
 import { v4 as uuidv4 } from 'uuid'
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { keywordProcessor } from './keywordProcessor'
+import { useNavigate } from 'react-router-dom'
 
 interface SearchKeywordsBarProps {
     validParams: Record<string, string>
@@ -8,20 +9,48 @@ interface SearchKeywordsBarProps {
 
 const SearchKeywordsBar: React.FC<SearchKeywordsBarProps> = ({ validParams }) => {
     const [searchKeywords, setSearchKeyword] = useState<Record<string, string>>(validParams)
+    const navigate = useNavigate()
+    const firstRender = useRef(true)
+
+    const handleRemoveSearchKey = (keyword: string): void => {
+        const { [keyword]: keyToDelete, ...rest } = searchKeywords
+        setSearchKeyword(rest)
+    }
+
+    useEffect(() => {
+        if (firstRender.current) {
+            firstRender.current = false
+        } else {
+            let newQueryString = '/watches?'
+            Object.entries(searchKeywords).forEach((keyword, index) => {
+                index === 0
+                    ? (newQueryString += `${encodeURIComponent(keyword[0])}=${encodeURIComponent(keyword[1])}`)
+                    : (newQueryString += `&${encodeURIComponent(keyword[0])}=${encodeURIComponent(keyword[1])}`)
+            })
+            console.log(newQueryString)
+            navigate(newQueryString)
+        }
+    }, [searchKeywords])
 
     return (
         <div>
             <ul>
-                {Object.entries(searchKeywords).map((searchKeyword) => (
-                    <li
-                        onClick={() => {
-                            handleRemoveSearchKey(searchKeyword[0])
-                        }}
-                        key={uuidv4()}
-                    >
-                        {keywordProcessor(searchKeyword[0], searchKeyword[1], searchKeywords)}
-                    </li>
-                ))}
+                {Object.entries(searchKeywords).map((searchKeyword) => {
+                    // if searchKeywoerd is equal to minPrice return null
+                    if (keywordProcessor(searchKeyword[0], searchKeyword[1], searchKeywords) !== null) {
+                        return (
+                            <li
+                                onClick={() => {
+                                    handleRemoveSearchKey(searchKeyword[0])
+                                }}
+                                key={uuidv4()}
+                            >
+                                {keywordProcessor(searchKeyword[0], searchKeyword[1], searchKeywords)}
+                            </li>
+                        )
+                    }
+                    return null
+                })}
             </ul>
         </div>
     )
--- src/components/SearchKeywordsBar/keywordProcessor.ts
+++ src/components/SearchKeywordsBar/keywordProcessor.ts
@@ -1,18 +1,22 @@
 import _ from 'lodash'
 
-const keywordProcessor = (keyword: string, value: string, searchKeywords: Record<string, string>): string => {
+const keywordProcessor = (keyword: string, value: string, searchKeywords: Record<string, string>): string | null => {
     if (keyword === 'gender') {
         if (_.lowerCase(value) === 'male') {
             return "Men's"
         } else {
             return "Women's"
         }
+    } else if (keyword === 'brand') {
+        return `Brand: ${value}`
     } else if (keyword === 'maxPrice') {
         if (!('minPrice' in searchKeywords)) {
             return `Up to ${parseInt(value).toLocaleString()} USD`
         } else {
             return `${parseInt(searchKeywords.minPrice).toLocaleString()} USD - ${parseInt(value).toLocaleString()} USD`
         }
+    } else if (keyword === 'minPrice') {
+        return null
     } else if (
         keyword === 'powerReserve' ||
         keyword === 'caseSize' ||
