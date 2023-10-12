e3be3feb766cf1b52cdb9761aa20492cdea34bdb
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-10-12 14:23:50 -0400

    Add searchQueryConstructor

--- src/pages/Watches/searchQueryConstructor.ts
+++ src/pages/Watches/searchQueryConstructor.ts
@@ -1,9 +1,20 @@
-import { ISearch } from '../../types/SearchInterface'
+import { type ISearch } from '../../types/SearchInterface'
 
-const searchQueryConstructor = (search: ISearch): void => {
+const searchQueryConstructor = (search: ISearch): string => {
     const location = window.location
-    const host = location.host
-    const url = `${host}/watches?`
+    let query = ''
+
+    // Generate string query
+    Object.entries(search).forEach(([key, value]) => {
+        if (search[key as keyof ISearch] !== '') {
+            if (query === '') {
+                query += `${key}=${value}`
+            } else {
+                query += `&${key}=${value}`
+            }
+        }
+    })
+    return `${location.host}/watches?${query}`
 }
 
 export { searchQueryConstructor }
