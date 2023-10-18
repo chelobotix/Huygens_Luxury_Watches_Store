c788b27a5448a74a129ca8b63c43defa443538b6
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-10-18 15:22:49 -0400

    fix linter2

--- src/helpers/getQueryString.ts
+++
@@ -1,9 +0,0 @@
-import { useSearchParams } from 'react-router-dom'
-import { readQueryString } from './readQueryString'
-
-const getQueryString = () => {
-    const [searchParams] = useSearchParams()
-    return readQueryString(searchParams)
-}
-
-export { getQueryString }
