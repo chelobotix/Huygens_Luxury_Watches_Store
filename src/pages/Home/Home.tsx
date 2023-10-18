962d511805c501bbd368a41ef13aa088c203fe8b
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-10-18 16:00:14 -0400

    Add Tailwind

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,7 +5,9 @@ import { v4 as uuidv4 } from 'uuid'
 const Home: React.FC = () => {
     const { watchesData } = useAppSelector((state) => state.watch)
     return (
-        <ul>{watchesData?.watches.map((watch) => <WatchCard key={uuidv4()} watch={watch} />)}</ul>
+        <>
+            <ul>{watchesData?.watches.map((watch) => <WatchCard key={uuidv4()} watch={watch} />)}</ul>
+        </>
     )
 }
 export { Home }
