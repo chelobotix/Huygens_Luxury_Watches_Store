1a3640f1ff4023cbf9199038a4f89cf3789bdfb6
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-11-03 14:09:32 -0400

    Delete unused custom hooks

--- src/hooks/useDetectScreenSize.ts
+++
@@ -1,9 +0,0 @@
-import { createTheme, useMediaQuery } from '@mui/material'
-
-const useDetectScreenSize = (direction: string, size: string): boolean => {
-    const theme = createTheme()
-    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
-    return isSmallScreen
-}
-
-export { useDetectScreenSize }
